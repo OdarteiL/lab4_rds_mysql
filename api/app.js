b79e38c1404f655b65ac11826499534f761b6bf1
OdarteiL <odarteilamptey.ml@gmail.com>
2025-05-05 08:57:55 +0000

    edited the route

--- api/app.js
+++ api/app.js
@@ -5,6 +5,12 @@ const routes = require('./routes');
 app.use(express.json());
 app.use('/api', routes);
 
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the MySQL RDS Lab API. Visit /top-customers, /monthly-sales, and other endpoints for insights.');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
